39cd3366996f4645d8b25a45167fe7259df707ac
IslamAlsaadawy <islamalsaadawy12@gmail.co>
2023-10-24 23:48:15 +0300

    fix: fixed naming

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int vectorSummition(vector<int> vectorToBeSummed){
+int getSum(vector<int> vectorToBeSummed){
     int resultOfSum = 0;
     for(auto iterator:vectorToBeSummed ){
         resultOfSum += iterator;
@@ -11,9 +11,10 @@ int vectorSummition(vector<int> vectorToBeSummed){
     return resultOfSum;
 }
 
+
 int main(){
     vector<int> testVector = {1,2,3,4,5,6,7,8,9,10};
-    cout << vectorSummition(testVector);
+    cout << getSum(testVector);
 
 
 
