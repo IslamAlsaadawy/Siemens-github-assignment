31e8f5b8b5eaa6bd5e075bd8ec608336afa7080d
IslamAlsaadawy <islamalsaadawy12@gmail.co>
2023-10-24 23:46:20 +0300

    Feat: Get average vector

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
@@ -11,9 +12,20 @@ int vectorSummition(vector<int> vectorToBeSummed){
     return resultOfSum;
 }
 
+int getAverage(vector<int> vectorToBeAveraged){
+    int resultOfSum = vectorSummition(vectorToBeAveraged);
+    int numberOfElementsInVector = vectorToBeAveraged.size();
+    int resultOfAverage = resultOfSum/numberOfElementsInVector;
+    return resultOfAverage;
+}
+
 int main(){
-    vector<int> testVector = {1,2,3,4,5,6,7,8,9,10};
-    cout << vectorSummition(testVector);
+    vector<int> sumVector = {1,2,3,4,5,6,7,8,9,10};
+    cout << vectorSummition(sumVector) << "\n";
+
+    
+
+
 
 
 
